eef8d2e54c33944af5fa393ae35f58fc8a76f217
Alessandro Mulloni <a.mulloni@gmail.com>
2016-08-03 15:00:33 +0200

    Fixed build.

--- sample/src/main/java/com/alessandromulloni/sample/MainActivity.java
+++ sample/src/main/java/com/alessandromulloni/sample/MainActivity.java
@@ -3,6 +3,8 @@ package com.alessandromulloni.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.alessandromulloni.swipebutton.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
